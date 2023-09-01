7675b694d35ac8571129c8e5e36f79b6b5b0cddb
zel-hub7 <zelalemyohannes62@gmail.com>
2023-09-01 06:39:38 -0700

    add routings

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
-
-
+import { Route, Routes } from "react-router-dom";
 import Home from "./pages/homePage/HomePage";
+
+const HatsPage = () => {
+  return (
+    <div>
+      <h1>This is Hats page</h1>
+    </div>
+  );
+};
+
 function App() {
   return (
     <div>
-       <Home />
+      <Routes>
+        <Route path="/hats" element={<HatsPage />} />
+        <Route path="/" element={<Home />} />
+      </Routes>
     </div>
   );
 }
